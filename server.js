dac8d3ace1aef3ac299b3706296ab4d22e1998f6
Misha Martin <mishanicolemartin@gmail.com>
2021-10-27 09:33:04 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const path = require('path')
+const Rollbar = require('rollbar')
+
+let rollbar = new Rollbar({
+    accessToken: '',
+    captureToken: true,
+    captureUnhandledRejections: true
+})
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully')
+})
+
+const port = process.env.PORT || 4545
+
+app.listen(port, () => console.log(`Take us to warp ${port}!`))
